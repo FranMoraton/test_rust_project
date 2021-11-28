0c655d8d6dbae5946667e8f58ed1727667f3c43a
FranMoraton <famoraton@gmail.com>
2021-11-28 21:48:03 +0100

    add definitions to fn

--- src/main.rs
+++ src/main.rs
@@ -61,4 +61,93 @@ fn main() {
     println!("{:p}", &v);
     println!("{:?}", std::mem::size_of_val(&output));
     println!("{:?}", std::mem::size_of_val(&v));
+
+    // Instantiate a MouseClick struct and bind the coordinate values
+    let click = MouseClick { x: 100, y: 250 };
+    println!("Mouse click location: {}, {}", click.x, click.y);
+        
+    // Instantiate a KeyPress tuple and bind the key values
+    let keys = KeyPress(String::from("Ctrl+"), 'N');
+    println!("\nKeys pressed: {}{}", keys.0, keys.1);
+        
+    // Instantiate WebEvent enum variants
+    // Set the boolean page Load value to true
+    let we_load = WebEvent::WELoad;
+    // Set the WEClick variant to use the data in the click struct
+    let we_click = WebEvent::WEClick(click);
+    // Set the WEKeys variant to use the data in the keys tuple
+    let we_key = WebEvent::WEKeys(keys);
+        
+    // Print the values in the WebEvent enum variants
+    // Use the {:#?} syntax to display the enum structure and data in a readable form
+    println!("\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}", we_load, we_click, we_key);
+
+    let num = 25;
+    println!("{} divided by 5 = {}", num, divide_by_5(num));
+
+    // Order three cars
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);    
+    
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+}
+
+//derive debug macro allows to print structs
+#[derive(Debug)]
+enum WebEvent {
+    // An enum variant can be like a unit struct without fields or data types
+    WELoad,
+    // An enum variant can be like a tuple struct with data types but no named fields
+    WEKeys(KeyPress),
+    // An enum variant can be like a classic struct with named fields and their data types
+    WEClick(MouseClick)
+}
+
+// Define a tuple struct
+#[derive(Debug)]
+struct KeyPress(String, char);
+
+// Define a classic struct
+#[derive(Debug)]
+struct MouseClick { x: i64, y: i64 }
+
+fn divide_by_5(num: u32) -> u32 {
+    if(0 == num) {
+        // Return early
+        return 0;
+    }
+    num / 5
+}
+
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    // todo!("Fix enum definition so code compiles");
+    Manual,
+    SemiAuto,
+    Automatic
+}
+
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+
+    // Use the values of the input arguments
+    // All new cars always have zero mileage
+    return Car{
+        color: color,
+        transmission: transmission,
+        convertible: convertible,
+        mileage: 0,
+    };
 }
