a269d5d028d66283f234c6679c3afe33d54b9bdc
FranMoraton <famoraton@gmail.com>
2021-12-12 12:07:01 +0100

    &dyn Trait vs &impl Trait?

--- src/traits.rs
+++ src/traits.rs
@@ -2,15 +2,27 @@ pub fn main() {
     let circle_declaration = Circle { radius: 2.0};
     let rectangle_declaration = Rectangle { height: 2.0, width: 5 as f64};
 
+    println!("pointer to struct {:p}", &circle_declaration);
     print_area(&circle_declaration);
+    print_area_dyn(&circle_declaration);
+
+    println!("pointer to struct {:p}", &rectangle_declaration);
     print_area(&rectangle_declaration);
+    print_area_dyn(&rectangle_declaration);
+
 }
 
 trait Area {
     fn calculate_area(&self) -> f64;
 }
 
-fn print_area(area: &dyn Area) {
+fn print_area(area: &impl Area) {
+    println!("pointer function area {:p}", &area);
+    println!("{}", area.calculate_area());
+}
+
+fn print_area_dyn(area: &dyn Area) {
+    println!("pointer function dyn area {:p}", &area);
     println!("{}", area.calculate_area());
 }
 
